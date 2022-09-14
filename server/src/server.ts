dd8300cd35b62fb9c6042f85b282b9b701d8a15d
lucaspires-source <pires.lucas94@gmail.com>
2022-09-14 14:28:43 -0300

    using typescript

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+
+
+const app = express()
+
+app.get('/ads', (req, res) =>{
+    return res.json([
+        {id: 1, name: 'AD 1'},
+        {id: 2, name: 'AD 2'},
+        {id: 3, name: 'AD 3'},
+        {id: 4, name: 'AD 4'},
+        {id: 5, name: 'AD 5'},
+    ])
+})
+
+app.listen(3333, () => { 
+    console.log('Listening on http://localhost:3333')
+})

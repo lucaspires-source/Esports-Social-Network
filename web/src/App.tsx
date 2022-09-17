f41f70ae6e0d40c91e44832e271a1072184c8aba
lucaspires-source <pires.lucas94@gmail.com>
2022-09-17 16:40:44 -0300

    finishing home layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,121 @@
 import './styles/main.css'
+import logo from './assets/logo.svg'
+import {MagnifyingGlassPlus} from 'phosphor-react'
 function App() {
   return (
-    <div className="w-8 h-8 bg-violet-500">
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logo} alt="logo" />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{' '}
+        <span className="bg-gradient text-transparent bg-clip-text">duo</span>{' '}
+        está aqui
+      </h1>
 
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a
+          href=""
+          target=""
+          rel=""
+          className="relative rounded-lg overflow-hidden"
+        >
+          <img
+            src="https://howlongtobeat.com/games/5203_League_of_Legends.jpg"
+            alt=""
+          />
+          <div className="w-full pt-16 pb-4 px-4 bg-gameGradient absolute bottom-0 left-0 right-0">
+            <strong className=" text-white font-bold block">Teste</strong>
+            <span className="text-zinc-300 text-sm block mt-1">teste</span>
+          </div>
+        </a>
+        <a
+          href=""
+          target=""
+          rel=""
+          className="relative rounded-lg overflow-hidden"
+        >
+          <img
+            src="https://howlongtobeat.com/games/5203_League_of_Legends.jpg"
+            alt=""
+          />
+          <div className="w-full pt-16 pb-4 px-4 bg-gameGradient absolute bottom-0 left-0 right-0">
+            <strong className=" text-white font-bold block">Teste</strong>
+            <span className="text-zinc-300 text-sm block mt-1">teste</span>
+          </div>
+        </a>
+        <a
+          href=""
+          target=""
+          rel=""
+          className="relative rounded-lg overflow-hidden"
+        >
+          <img
+            src="https://howlongtobeat.com/games/5203_League_of_Legends.jpg"
+            alt=""
+          />
+          <div className="w-full pt-16 pb-4 px-4 bg-gameGradient absolute bottom-0 left-0 right-0">
+            <strong className=" text-white font-bold block">Teste</strong>
+            <span className="text-zinc-300 text-sm block mt-1">teste</span>
+          </div>
+        </a>
+        <a
+          href=""
+          target=""
+          rel=""
+          className="relative rounded-lg overflow-hidden"
+        >
+          <img
+            src="https://howlongtobeat.com/games/5203_League_of_Legends.jpg"
+            alt=""
+          />
+          <div className="w-full pt-16 pb-4 px-4 bg-gameGradient absolute bottom-0 left-0 right-0">
+            <strong className=" text-white font-bold block">Teste</strong>
+            <span className="text-zinc-300 text-sm block mt-1">teste</span>
+          </div>
+        </a>
+        <a
+          href=""
+          target=""
+          rel=""
+          className="relative rounded-lg overflow-hidden"
+        >
+          <img
+            src="https://howlongtobeat.com/games/5203_League_of_Legends.jpg"
+            alt=""
+          />
+          <div className="w-full pt-16 pb-4 px-4 bg-gameGradient absolute bottom-0 left-0 right-0">
+            <strong className=" text-white font-bold block">Teste</strong>
+            <span className="text-zinc-300 text-sm block mt-1">teste</span>
+          </div>
+        </a>
+        <a
+          href=""
+          target=""
+          rel=""
+          className="relative rounded-lg overflow-hidden"
+        >
+          <img
+            src="https://howlongtobeat.com/games/5203_League_of_Legends.jpg"
+            alt=""
+          />
+          <div className="w-full pt-16 pb-4 px-4 bg-gameGradient absolute bottom-0 left-0 right-0">
+            <strong className=" text-white font-bold block">Teste</strong>
+            <span className="text-zinc-300 text-sm block mt-1">teste</span>
+          </div>
+        </a>
+      </div>
+      <div className="pt-1 bg-gradient self-stretch rounded-lg overflow-hidden mt-8">
+        <div className="bg-[#2a2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">
+              Não encontrou seu duo?
+            </strong>
+            <span className="text-zinc-400">
+              Publique um anúncio para encontrar novos players !
+            </span>
+          </div>
+          <button className='py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center gap-3'><MagnifyingGlassPlus size={24}/> Publicar anúncio </button>
+        </div>
+      </div>
     </div>
   )
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,7 +5,16 @@ module.exports = {
     './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans:['Inter', 'sans-serif']
+      },
+      backgroundImage:{
+        background: "url('/backgroundGalaxy.png')",
+        gradient: 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
+        gameGradient: 'linear-gradient(180deg,  rgba(0,0,0,0), rgba(0,0,0,0) 0%, rgba(0,0,0,0.9) 67.08%)'
+      }
+    },
   },
   plugins: [],
 }

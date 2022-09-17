5460bf6a8170ff99a657edeec3ad421714a0e8ae
lucaspires-source <pires.lucas94@gmail.com>
2022-09-17 19:11:20 -0300

    finalizing home screen on mobile app

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from 'react'
+import { View, Image, FlatList } from 'react-native'
 
-import { styles } from './styles';
-
+import { styles } from './styles'
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { Header } from '../../components/Header'
+import { GameCard } from '../../components/GameCard'
+import { GAMES } from '../../utils/games'
 export function Home() {
   return (
     <View style={styles.container}>
-
+      <Image source={logoImg} style={styles.logo} />
+      <Header
+        title="Encontre seu duo!"
+        subtitle="Selecione o jogo que deseja jogar..."
+      />
+      <FlatList
+        contentContainerStyle={styles.contentList}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+      />
     </View>
-  );
+  )
 }

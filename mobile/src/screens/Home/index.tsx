5bc8a3d2e04ea07a0b97538298bfbce52fd5f5f3
lucaspires-source <pires.lucas94@gmail.com>
2022-09-23 10:52:01 -0300

    fetching data from our api

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { View, Image, FlatList } from 'react-native'
 
 import { styles } from './styles'
 import logoImg from '../../assets/logo-nlw-esports.png'
 import { Header } from '../../components/Header'
-import { GameCard } from '../../components/GameCard'
-import { GAMES } from '../../utils/games'
+import { GameCard, GameCardProps } from '../../components/GameCard'
+
+
+
 export function Home() {
+const [games,setGames] = useState<GameCardProps[]>([])
+
+
+  useEffect(() =>{
+    fetch('http://192.168.15.110:3333/games')
+    .then(response => response.json())
+     .then(data => { setGames(data)})
+     .catch(err => { throw  new Error(err); });
+  },[])
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
@@ -18,7 +29,7 @@ export function Home() {
         contentContainerStyle={styles.contentList}
         horizontal
         showsHorizontalScrollIndicator={false}
-        data={GAMES}
+        data={games}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <GameCard data={item} />}
       />
